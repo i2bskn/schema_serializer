eccca1b7fb5fb6e6cfdc846594d1314031c8ffbc
i2bskn <i2bskn@gmail.com>
2018-07-25 02:01:05 +0900

    add definition generator from openapi3_parser

---
+++ lib/schema_serializer/definition/from_openapi3_parser.rb
@@ -0,0 +1,33 @@
+require "openapi3_parser"
+
+class SchemaSerializer::Definition
+  module FromOpenapi3Parser
+    class << self
+      def build(document)
+        SchemaSerializer::Definition.new(schemas_to_hash(document.components.schemas))
+      end
+
+      private
+
+        def schemas_to_hash(schemas)
+          schemas.each_with_object({}) { |(name, node), obj|
+            obj[name.to_s] = node_to_hash(node)
+          }
+        end
+
+        def node_to_hash(node)
+          obj = { "type" => node.type, "nullable" => node.nullable? }
+          case obj["type"]
+          when "array"
+            obj["items"] = node_to_hash(node.items)
+          when nil
+            obj["required"] = node.required.node_data
+            obj["properties"] = node.properties.each_with_object({}) { |(name, node), obj|
+              obj[name.to_s] = node_to_hash(node)
+            }
+          end
+          obj
+        end
+    end
+  end
+end
