1d817d6df71a3ae5e83317f987089ebe8ec69a39
i2bskn <i2bskn@gmail.com>
2018-07-29 04:37:53 +0900

    Fix install generator

--- lib/generators/schema_serializer/install/templates/schema_serializer.rb
+++ lib/generators/schema_serializer/install/templates/schema_serializer.rb
@@ -1,5 +1,7 @@
 # Uncomment following if use OpenAPI 3 specification.
 # require "schema_serializer/definition/from_openapi3_parser"
-# SchemaSerializer.definition = SchemaSerializer::Definition::FromOpenapi3Parser.build(Rails.root.join("doc/schema.yml"))
+# SchemaSerializer.definition = SchemaSerializer::Definition::FromOpenapi3Parser.build(
+#   Openapi3Parser.load_file(Rails.root.join("doc/schema.yml"))
+# )
 
 SchemaSerializer.definition = YAML.load_file(Rails.root.join("doc/schema.yml"))
