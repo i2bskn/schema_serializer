bc3960865b6ad60e5963e74db13cdc9e34f84a1b
i2bskn <i2bskn@craftake.com>
2018-11-20 12:21:58 +0900

    Support object type in components/schemas

--- lib/schema_serializer/definition/from_openapi3_parser.rb
+++ lib/schema_serializer/definition/from_openapi3_parser.rb
@@ -20,7 +20,7 @@ class SchemaSerializer::Definition
           case obj["type"]
           when "array"
             obj["items"] = node_to_hash(node.items)
-          when nil
+          when obj["object"], nil
             obj["required"] = node.required.node_data
             obj["properties"] = node.properties.each_with_object({}) {|(name, property), properties|
               properties[name.to_s] = node_to_hash(property)
