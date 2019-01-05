e7ace75626445b003904899b29afde0ae81bcf97
i2bskn <i2bskn@craftake.com>
2019-01-06 05:47:23 +0900

    Bug fix

--- lib/schema_serializer/definition/from_openapi3_parser.rb
+++ lib/schema_serializer/definition/from_openapi3_parser.rb
@@ -20,8 +20,8 @@ class SchemaSerializer::Definition
           case obj["type"]
           when "array"
             obj["items"] = node_to_hash(node.items)
-          when obj["object"], nil
-            obj["required"] = node.required.node_data
+          when "object", nil
+            obj["required"] = node.required&.node_data || []
             obj["properties"] = node.properties.each_with_object({}) {|(name, property), properties|
               properties[name.to_s] = node_to_hash(property)
             }
