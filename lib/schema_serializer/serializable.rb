14b14546c6a0a59634a1a4ef48e390400162f861
i2bskn <i2bskn@craftake.com>
2019-10-11 14:54:35 +0900

    Bug fix

--- lib/schema_serializer/serializable.rb
+++ lib/schema_serializer/serializable.rb
@@ -3,7 +3,7 @@ class SchemaSerializer
     def serializer(*args)
       options = args.last.is_a?(Hash) ? args.pop : {}
       klass = args.first || options[:class]
-      (klass || "#{self.class.name}Serializer".safe_constantize || SchemaSerializer).new(self)
+      (klass || "#{self.class.name}Serializer".safe_constantize || SchemaSerializer).new(self, options)
     end
   end
 end
